6319b0ff4a239c7bfc0833e31bfd7f66771601cc
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-09-09 19:54:28 -0500

    Add concert routes for creation and editing

---
+++ src/app/new-concert/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConcertForm from '../../components/ConcertForm';
+
+export default function DeleteMe() {
+  return (
+    <div>
+      <ConcertForm />
+    </div>
+  );
+}
